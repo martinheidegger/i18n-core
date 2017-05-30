8b52f947e7b654cefa21ed85315eae988d7c588e
Martin Heidegger <martin.heidegger@gmail.com>
2017-05-30 23:37:42 +0900

    fix: properly escaping strings in simplified html rendering using ‘escape-html’

--- test/mustacheSimple.js
+++ test/mustacheSimple.js
@@ -4,10 +4,12 @@ var render = require('../lib/mustacheSimple').render
 
 test('Simple parameter replacement', function (t) {
   t.equals(render('hello {{x}}', {x: 'world'}), 'hello world')
+  t.equals(render('hello {{x}}', {x: '<world>'}), 'hello &lt;world&gt;')
   t.end()
 })
 
 test('additional brace parameter replacement', function (t) {
   t.equals(render('hello {{{x}}}', {x: 'world'}), 'hello world')
+  t.equals(render('hello {{{x}}}', {x: '<world>'}), 'hello <world>')
   t.end()
 })
